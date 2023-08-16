3bb29b4c07435fe48c198baa5512c0b00d811427
Oladayo 300 <olanipekunoladayo19@gmail.com>
2023-08-16 02:02:29 +0000

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- *  * main - print _putchar
- *   *
- *    * Return: (0)
- *     *
- *      */
+ * main - print _putchar
+ *
+ * Return: (0)
+ *
+ */
 int main(void)
 {
-		int i;
-			char c[] = "_putchar";
+	int i;
+	char c[] = "_putchar";
 
-				for (i = 0; i <= 7; i++)
-						{
-									_putchar(c[i]);
-										}
-					_putchar('\n');
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 
-						return (0);
+	return (0);
 }
